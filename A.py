6be014b50e49e951d0ef56f9c5493916fe748f28
elsy <kpp0193@gmail.com>
2024-07-01 20:43:27 +0900

    aaaaaaa

--- A.py
+++ A.py
@@ -1,4 +1,9 @@
 for i in range(1, 10):
     for j in range(1, 10):
-        print(i * j, end = " "")
-    print()
+        if j == 1:
+            print(i, "|", end = "")
+        if i * j < 10:
+            print(" ", end = "")
+        print(i * j, end = " ")
+        if j == 9:
+            print("|")
