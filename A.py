97cc7fed3d8f8cb80b78b57d74413141cd62bef1
elsy <kpp0193@gmail.com>
2024-07-01 20:40:24 +0900

    modify indent

--- A.py
+++ A.py
@@ -1,4 +1,7 @@
 for i in range(1, 10):
     for j in range(1, 10):
-        print(i * j, end = " "")
+        if i * j < 10:
+            print("", i * j, end = " ")
+        else:
+            print(i * j, end = " ")
     print()
